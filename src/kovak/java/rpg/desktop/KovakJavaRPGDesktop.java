9517ff451046830eac5a3fad6f120feccb6660d4
kealdor <32488803+kealdor@users.noreply.github.com>
2017-10-16 23:38:11 -0400

    Base Empty Project with game main menu Signed-off-by:kealdor <32488803+kealdor@users.noreply.github.com>

---
+++ src/kovak/java/rpg/desktop/KovakJavaRPGDesktop.java
@@ -0,0 +1,19 @@
+/*
+ * Initial Code Created by Joe Sipos aka Cealdor/Kealdor/Ryuhi/Ghost
+ * This program/game is distributed in the hope that it will be useful/enjoyable,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+package kovak.java.rpg.desktop;
+
+import kovak.java.rpg.desktop_screens.Title_Screen;
+
+public class KovakJavaRPGDesktop {
+
+    public static void main() {
+        Title_Screen MainMenu = new kovak.java.rpg.desktop_screens.Title_Screen();
+        MainMenu.main();
+    }
+
+}
